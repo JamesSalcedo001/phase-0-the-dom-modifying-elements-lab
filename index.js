9dd557a1d15ab45da3ca81c265b97a9acfa6edac
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-09-11 23:48:55 -0500

    commit tree 20aa07e8c6e65e3aa7eb917af463b145e8931bbf

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainId = document.getElementById("main");
+mainId.remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "JAMES is the champion"
+
